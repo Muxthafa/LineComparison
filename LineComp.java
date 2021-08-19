6ed7041c5c2a20b14ac248c50694cbd1a594c53c
muxthafa <musthafamohd0@gmail.com>
2021-08-19 09:56:20 +0530

    Added: welcome to line comparison problem

---
+++ LineComp.java
@@ -0,0 +1,10 @@
+package com.practiceproblem;
+
+public class LineComp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wecome to line comparison problem");
+	}
+
+}
