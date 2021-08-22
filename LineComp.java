0faec73c6a560fe5f63c82a777961f7a434d0e4f
muxthafa <musthafamohd0@gmail.com>
2021-08-22 18:54:06 +0530

    Added: object oriented concept used for line comparison problem

--- LineComp.java
+++ LineComp.java
@@ -3,8 +3,22 @@ package com.practiceproblem;
 import java.util.Scanner;
 
 public class LineComp {
-	public static int linelength(int x1,int y1,int x2,int y2) {
-		return (int) Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+	//class members
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+		
+	//parameterized constructor
+	public LineComp(int x1,int y1,int x2,int y2)
+	{
+		this.x1=x1;
+		this.y1=y1;
+		this.x2=x2;
+		this.y2=y2;
+	}
+	public double linelength() {
+		return Math.pow(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)),0.5);
 	}
 	
 	public static void main(String[] args) {
@@ -20,8 +34,9 @@ public class LineComp {
 		sc.nextLine();
 		int y2=sc.nextInt();
 		sc.nextLine();
-		int len1 = linelength(x1,y1,x2,y2);
-		System.out.println("length of line 1: "+len1);
+//		int len1 = linelength(x1,y1,x2,y2);
+		//object is created with parameters obtained by user
+		LineComp len1 = new LineComp(x1,y1,x2,y2);
 		System.out.println("Enter the end points of the line 2:");
 		x1=sc.nextInt();
 		sc.nextLine();
@@ -31,14 +46,33 @@ public class LineComp {
 		sc.nextLine();
 		y2=sc.nextInt();
 		sc.nextLine();
-		int len2 = linelength(x1,y1,x2,y2);
-		System.out.println("length of line 2: "+len2);
-		if(len1 == len2)
-			System.out.println("The lines are equal!!");
-		else if(len1 > len2)
-			System.out.println("The line 1 is greater than line 2");
+		//object is created with parameters obtained by user
+		LineComp len2 = new LineComp(x1,y1,x2,y2);
+//		int len2 = linelength(x1,y1,x2,y2);
+		
+		double lengthofline;
+		lengthofline = len1.linelength();
+		System.out.println("The length of the line 1  is "+lengthofline);
+		
+		//converted to object
+		Double length_line1 = new Double(lengthofline);
+		lengthofline = len2.linelength();
+		
+		System.out.println("The length of the line2  is "+lengthofline);
+		//length is converted to an object
+		Double length_line2 = new Double(lengthofline);
+		
+		if(length_line1.equals(length_line2))			//equals method used to check equility two objects
+			System.out.println("The lines are equal");
 		else
-			System.out.println("Line 1 is lesser than line 2");
+		{
+			//compareTo method used to compare two objects
+			int result = length_line1.compareTo(length_line2);
+			if(result == 1)
+				System.out.println("line1 is greater than line2");
+			else if(result==-1)
+				System.out.println("Line2 is greater than line1");
+		}
 	}
 
 }
